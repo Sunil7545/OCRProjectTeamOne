324ca7b415c79cd0e8450377d933968e09d7058a
dhrumpy <abhishekparida22@gmail.com>
2019-12-30 03:08:25 +0530

    commit message from terminal

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
 
-print('Hellp World!!)
+# # Simple image to string
+print(pytesseract.image_to_string(Image.open('1.jpg')))
+
