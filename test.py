27697d90c433d8dbd1049d4c7b566305b5e0dfde
dhrumpy <abhishekparida22@gmail.com>
2020-01-01 06:24:56 +0530

    collecting the data pertaining to the keyword name

--- test.py
+++ test.py
@@ -3,7 +3,21 @@ try:
 except ImportError:
     import Image
 import pytesseract
+# pytesseract.pytesseract.tesseract_cmd
+# import PyPDF2
+# from pdf2image import convert_from_path
 
-# # Simple image to string
-print(pytesseract.image_to_string(Image.open('1.jpg')))
+def ocer_core(inputImage):
+    text = pytesseract.image_to_string(inputImage)
+    return text
 
+fileName = 'shweta.jpg' # file taken from Akriti
+inputData = Image.open(fileName)
+textData = ocer_core(inputData)
+print(type(inputData),type(textData))
+# data=open('img2txt.txt','w+')
+# data.write(textData)
+# data.close()
+rawData = textData.splitlines()
+indices = [idx for idx, value in enumerate(rawData) if 'Name' in value] # data indices consisting of names
+data = [rawData[i] for i in indices]
